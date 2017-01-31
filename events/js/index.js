0a99097e326d3837073122ad1948e54fd1bca688
Abhilash Verma <vermaabhilash70@gmail.com>
2017-02-01 00:05:05 +0530

    Events tabbed by sidebar

--- events/js/index.js
+++ events/js/index.js
@@ -1,29 +1,17 @@
 
 $(document).ready(function () {
-    
-//     $('#nav').children('li').first().children('a').addClass('active')
-//         .next().addClass('is-open').show(500);
-        
-//     $('#nav').on('click', 'li > a', function() {
-        
-//       if (!$(this).hasClass('active')) {
-
-//         $('#nav .is-open').removeClass('is-open').hide(500);
-//         $(this).next().toggleClass('is-open').toggle(500);
-          
-//         $('#nav').find('.active').removeClass('active');
-//         $(this).addClass('active');
-//       } else {
-//         $('#nav .is-open').removeClass('is-open').hide(500);
-//         $(this).removeClass('active');
-//       }
-//    });
-
     $('#nav li').click(function(){
-        $(this).removeClass('stacked').addClass('open').siblings().removeClass('open').addClass('stacked');
+        $(this).removeClass('stacked').addClass('open').siblings().filter(".enabled").removeClass('open').addClass('stacked');
     });
     $('#nav li .escape').click(function(e){
-        $(this).parent().removeClass('open').siblings().removeClass('stacked');
+        $(this).parent().removeClass('open').siblings().filter(".enabled").removeClass('stacked');
         e.stopPropagation();
     });
+    $('#sidebar > ul > li').click(function(){
+        $(this).addClass('active').siblings().removeClass('active');
+        $('#nav > li.enabled').removeClass('enabled');
+        var category = ($(this).data('category'));
+        console.log(category);
+        $("#nav > li[data-category=" + category + "]").addClass('enabled');
+    })
 });
